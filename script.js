9603cd3b7bbed2a915adbce4bc087a9c050f44ae
Patrick-Santiago <patrick.santiago@gmail.com>
2022-10-25 15:53:15 -0400

    Add pseudocode comments

---
+++ script.js
@@ -0,0 +1,24 @@
+// When a user inputs either Rock, Paper, Scissors
+
+// Computer will randomly select Rock, Paper, Scissors
+
+// If user and computer select same string, then print “Tie. Try Again”
+
+//  user selects Rock and computer selects Paper, print “You lose! Paper beats Rock”
+
+// If user selects Rock and computer selects Scissors, print “You Win! Rock beats Scissors”
+
+// If user selects Paper and computer selects Rock, print “You win! Paper beats Rock!”
+
+// If user selects Paper and computer selects Scissors, print “You Lose! Scissors beat Paper!”
+
+// If user selects Scissors and computer selects Rock, print “You lose! Rock beats Scissors!”
+
+// If user selects Scissors and computer selects Paper, print “You Win! Scissors beat Paper!”
+
+//
+
+// Keep tally of a 5 round game
+
+// Print user outcome of 5 round game, You Win! Or You Lose!
+
