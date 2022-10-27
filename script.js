b2d9feed85becfd4614f36115801d392d4559d06
Patrick-Santiago <patrick.santiago@gmail.com>
2022-10-27 12:30:07 -0400

    Erase previous code, add revised pseudocode.

--- script.js
+++ script.js
@@ -1,56 +1,13 @@
-// Computer will randomly select Rock, Paper, Scissors
-function getComputerChoice(string) {
-    const randomChoice = Math.floor(Math.random() * string.length);
-    const item = string[randomChoice];
-    return item;
-}
-
-const compOptions = ["Rock","Paper","Scissors"];
-const computerSelection = getComputerChoice(compOptions);
-
-// When a user inputs either Rock, Paper, Scissors
-function getPlayerChoice(selection) {
-    const playerOption = prompt("What is your selection?");
-   if (playerOption === ("Rock" || "Paper" || "Scissors")) {
-    alert("Thanks for your choice");
-   } else {
-    alert("Please enter valid option");
-   }
-}
-
-const playerOptions = ["Rock","Paper","Scissors"];
-//const playerSelection = getPlayerChoice(playerOptions);
-
-function playRound(playerSelection,computerSelection) {
- if (playerSelection === computerSelection) {
-    return alert("It's a Tie! Try again");
- } else {
-    return alert("Who won?");
- }
-}
-
-const playerSelection = "Rock";
-console.log(playRound(playerSelection, computerSelection));
+// Computer (Opponent) will randomly choose between Rock, Paper or Scissors
+// User will input their selection between Rock, Paper or Scissors
+// Based on the rules of rock, paper, scissors
+// If computer and user select the same item then return a message of “It’s a Tie, Try Again!”
+// If user selects Rock and computer selects Scissors then return message “Rock beats Scissors. You Win!”
+// If user selects Paper and computer selects Rock then return message “Paper beats Rock. You Win!”
+// If user selects Scissors and computer selects Paper then return message “Scissors beats Paper. You Win!”
+// Or else return message “[computerSelection} beats [playerSelection] You Lost.”
+// Record tally of a 5 round game
+// Print user outcome of 5 round game with a message of the winner
 
 
-
-// If user and computer select same string, then print “Tie. Try Again”
-
-// If user selects Rock and computer selects Paper, print “You lose! Paper beats Rock”
-
-// If user selects Rock and computer selects Scissors, print “You Win! Rock beats Scissors”
-
-// If user selects Paper and computer selects Rock, print “You win! Paper beats Rock!”
-
-// If user selects Paper and computer selects Scissors, print “You Lose! Scissors beat Paper!”
-
-// If user selects Scissors and computer selects Rock, print “You lose! Rock beats Scissors!”
-
-// If user selects Scissors and computer selects Paper, print “You Win! Scissors beat Paper!”
-
-//
-
-// Keep tally of a 5 round game
-
-// Print user outcome of 5 round game, You Win! Or You Lose!
 
