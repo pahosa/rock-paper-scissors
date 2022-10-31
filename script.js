11f689a9476dba847a8ff04ad72c7fe8b27a63f9
Patrick-Santiago <patrick.santiago@gmail.com>
2022-10-31 09:49:57 -0400

    Add v3 of pseudocode.

---
+++ script.js
@@ -0,0 +1,33 @@
+// Computer randomly selects Rock, Paper, Scissors
+
+
+
+// User inputs selection of Rock, Paper, Scissors (case insensitive)
+
+
+
+// If user inputs alternative selection or invalid selection, ask to input selection again
+
+
+
+// If user and computer select the same selection 
+// return message playerSelection vs computerSelection. It’s a Tie!
+
+
+
+// If user selects Rock and computer selects Scissors OR user selects Paper and computer selects Rock OR user selects Scissors and computer selects Paper 
+// return message playerSelection beats computerSelection. You Win!
+
+
+
+// Else return message computerSelection beats playerSelection. You Lose!
+
+
+
+// Create 5 round game 
+
+
+
+// Print outcomes of each game and overall winner of the 5 rounds
+
+
