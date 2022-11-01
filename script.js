c61dbd0eb65de015998e47c7478a53e217c29a35
Patrick-Santiago <patrick.santiago@gmail.com>
2022-11-01 16:36:32 -0400

    Add validation to getPlayerChoice, addd comment to revise using loops in future.

--- script.js
+++ script.js
@@ -10,29 +10,29 @@ function getComputerChoice() {
 const computerSelection = getComputerChoice()
 
 // User inputs selection of Rock, Paper, Scissors (case insensitive)
+// If user inputs alternative selection or invalid selection, ask to input selection again
+// REVISIT VALIDATION WITH LOOPS IN FUTURE
 function getPlayerChoice() {
     const playerChoice = prompt("Choose Rock, Paper, or Scissors");
-    return playerChoice;
+    if (playerChoice === "Rock" || playerChoice === "Paper" || playerChoice === "Scissors") {
+        return playerChoice;
+     } else {
+    const playerChoiceAgain = prompt("Please try again. Choose Rock, Paper, or Scissors");
+        if (playerChoiceAgain === "Rock" || playerChoiceAgain === "Paper" || playerChoiceAgain === "Scissors") {
+        return playerChoiceAgain;
+     } else {
+        alert("Input Invalid. Please Try Again.")
+     }
+    }    
 }
 
 const playerSelection = getPlayerChoice()
 
-console.log(playerSelection);
-
-// If user inputs alternative selection or invalid selection, ask to input selection again
-
-
-
+// create game to compare the two selections and determine a winner
 // If user and computer select the same selection 
 // return message playerSelection vs computerSelection. It’s a Tie!
-
-
-
 // If user selects Rock and computer selects Scissors OR user selects Paper and computer selects Rock OR user selects Scissors and computer selects Paper 
 // return message playerSelection beats computerSelection. You Win!
-
-
-
 // Else return message computerSelection beats playerSelection. You Lose!
 
 
