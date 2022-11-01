eeff4a039c61fa0179f22623c4f34070f3d663c5
Patrick-Santiago <patrick.santiago@gmail.com>
2022-11-01 16:50:01 -0400

    Add playRound function.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const array = ["Rock","Paper","Scissors"]
 function getComputerChoice() {
     const item = Math.floor(Math.random() * array.length);
     const compChoice = array[item];
+    return compChoice;
 }
 
 const computerSelection = getComputerChoice()
@@ -28,12 +29,24 @@ function getPlayerChoice() {
 
 const playerSelection = getPlayerChoice()
 
-// create game to compare the two selections and determine a winner
+// Create single round game to compare the two selections and determine a winner
 // If user and computer select the same selection 
 // return message playerSelection vs computerSelection. It’s a Tie!
 // If user selects Rock and computer selects Scissors OR user selects Paper and computer selects Rock OR user selects Scissors and computer selects Paper 
 // return message playerSelection beats computerSelection. You Win!
 // Else return message computerSelection beats playerSelection. You Lose!
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        alert(playerSelection + " vs. " + computerSelection + ". It's a TIE!")
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" ||
+    playerSelection === "Scissors" && computerSelection === "Paper") {
+        alert(playerSelection + " beats " +  computerSelection + ". You WIN!")
+    } else {
+        alert(computerSelection + " beats " +  playerSelection + ". You LOSE!")
+    }
+ }
+
+ console.log(playRound(playerSelection, computerSelection));
 
 
 
